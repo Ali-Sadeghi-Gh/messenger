924d96f45a7938832553d131495444f257a39787
Ali-Sadeghi-Gh <107436444+Ali-Sadeghi-Gh@users.noreply.github.com>
2024-06-02 00:07:10 +0330

    add login and signin DTOs

---
+++ src/main/java/ir/mohaymen/messenger/dto/LoginRequest.java
@@ -0,0 +1,17 @@
+package ir.mohaymen.messenger.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+    private String name;
+}
---
+++ src/main/java/ir/mohaymen/messenger/dto/SigninRequest.java
@@ -0,0 +1,15 @@
+package ir.mohaymen.messenger.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SigninRequest {
+    private String username;
+    private String password;
+}
