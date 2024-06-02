0ca024088e9bc88353b37bfaaf0a6fd9075190b3
Ali-Sadeghi-Gh <107436444+Ali-Sadeghi-Gh@users.noreply.github.com>
2024-06-03 00:58:23 +0330

    transfer AuthenticationManager bean

--- src/main/java/ir/mohaymen/messenger/security/SecurityConfiguration.java
+++ src/main/java/ir/mohaymen/messenger/security/SecurityConfiguration.java
@@ -3,9 +3,7 @@ package ir.mohaymen.messenger.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -41,10 +39,5 @@ public class SecurityConfiguration {
                 .anonymous()
                 .anyRequest()
                 .authenticated();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
     }
 }
