39a2813e72387f01c4aa9623c581d878611e2839
Ali-Sadeghi-Gh <107436444+Ali-Sadeghi-Gh@users.noreply.github.com>
2024-06-03 19:00:25 +0330

    adding getChats to MessageController

--- src/main/java/ir/mohaymen/messenger/controllers/MessageController.java
+++ src/main/java/ir/mohaymen/messenger/controllers/MessageController.java
@@ -1,23 +1,29 @@
 package ir.mohaymen.messenger.controllers;
 
+import ir.mohaymen.messenger.dto.ChatResponse;
 import ir.mohaymen.messenger.dto.MessageRequest;
+import ir.mohaymen.messenger.services.ChatService;
 import ir.mohaymen.messenger.services.SendMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api")
 @RequiredArgsConstructor
 public class MessageController {
     private final SendMessageService sendMessageService;
+    private final ChatService chatService;
 
     @PostMapping(path = "sendMessage")
     public ResponseEntity<?> sendMessage(@RequestBody MessageRequest request) {
         return sendMessageService.sendMessage(request);
     }
 
+    @GetMapping(path = "getChats")
+    public ResponseEntity<List<ChatResponse>> getChats() {
+        return chatService.getChats();
+    }
 }
