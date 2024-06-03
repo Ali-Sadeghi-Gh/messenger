7cda0e90e9234b188f88f4bbcd39524707a0dab0
Ali-Sadeghi-Gh <107436444+Ali-Sadeghi-Gh@users.noreply.github.com>
2024-06-03 20:02:45 +0330

    adding getUnreadMessages method

--- src/main/java/ir/mohaymen/messenger/controllers/MessageController.java
+++ src/main/java/ir/mohaymen/messenger/controllers/MessageController.java
@@ -2,8 +2,10 @@ package ir.mohaymen.messenger.controllers;
 
 import ir.mohaymen.messenger.dto.ChatResponse;
 import ir.mohaymen.messenger.dto.MessageRequest;
+import ir.mohaymen.messenger.dto.UnreadMessageResponse;
 import ir.mohaymen.messenger.services.ChatService;
 import ir.mohaymen.messenger.services.SendMessageService;
+import ir.mohaymen.messenger.services.UnreadMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import java.util.List;
 public class MessageController {
     private final SendMessageService sendMessageService;
     private final ChatService chatService;
+    private final UnreadMessageService unreadMessageService;
 
     @PostMapping(path = "sendMessage")
     public ResponseEntity<?> sendMessage(@RequestBody MessageRequest request) {
@@ -25,5 +28,10 @@ public class MessageController {
     @GetMapping(path = "getChats")
     public ResponseEntity<List<ChatResponse>> getChats() {
         return chatService.getChats();
+    }
+
+    @GetMapping(path = "getUnreadMessages/{addresseeId}")
+    public ResponseEntity<List<UnreadMessageResponse>> getUnreadMessages(@PathVariable Long addresseeId) {
+        return unreadMessageService.getUnreadMessages(addresseeId);
     }
 }
