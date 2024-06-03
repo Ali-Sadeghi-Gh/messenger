d984df753cd32b5ceced875ba1a567c9bf544584
Ali-Sadeghi-Gh <107436444+Ali-Sadeghi-Gh@users.noreply.github.com>
2024-06-03 18:35:01 +0330

    delete unnecessary repositories

--- src/main/java/ir/mohaymen/messenger/repositories/ChatRepository.java
+++
@@ -1,9 +0,0 @@
-package ir.mohaymen.messenger.repositories;
-
-import ir.mohaymen.messenger.entities.ChatEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatRepository extends JpaRepository<ChatEntity, Long> {
-}
--- src/main/java/ir/mohaymen/messenger/repositories/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package ir.mohaymen.messenger.repositories;
-
-import ir.mohaymen.messenger.entities.MessageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
-}
